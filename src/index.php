e1c9f9873ad89ac36145192a7aa69f2eba30c4ac
Emre <Emre.kartal@etu.uca.fr>
2022-11-19 16:21:09 +0100

    Réglage des problème du MVC

--- src/index.php
+++ src/index.php
@@ -7,11 +7,23 @@
 //chargement config
 require_once(__DIR__.'/config/config.php');
 
+//autolarder du cours
 //chargement autoloader pour autochargement des classes
-require_once(__DIR__.'/config/Autoload.php');
-Autoload::charger();
+//require_once(__DIR__.'/config/Autoload.php');
+//Autoload::charger();
 
-$cont = new Controleur();
+
+//autoloader conforme norme PSR-0
+require_once(__DIR__.'/config/SplClassLoader.php');
+$myLibLoader = new SplClassLoader('Controller', './');
+$myLibLoader->register();
+$myLibLoader = new SplClassLoader('config', './');
+$myLibLoader->register();
+$myLibLoader = new SplClassLoader('Modele', './');
+$myLibLoader->register();
+
+
+$cont = new \Controller\Controleur();
 
 
 ?> 
